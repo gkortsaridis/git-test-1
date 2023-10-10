d2f14f54aea036705d04c8204db32e74dc0b1acc
George Kortsaridis <geokor@amazon.co.uk>
2023-10-10 17:56:01 +0100

    Fixed typo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 public static void main(String[] args) {
-java.utli.Scanner scan = new java.util.Scanner(System.in);
+java.util.Scanner scan = new java.util.Scanner(System.in);
 System.out.println("Hello World!");
 System.out.println("What’s your name?");
 String s = scan.nextLine();
