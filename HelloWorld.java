091a20222cb458caee024769ceae7c56a715f661
George Kortsaridis <geokor@amazon.co.uk>
2023-10-10 17:54:43 +0100

    Added some user input

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 public class HelloWorld {
-    
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-
+public static void main(String[] args) {
+java.utli.Scanner scan = new java.util.Scanner(System.in);
+System.out.println("Hello World!");
+System.out.println("What’s your name?");
+String s = scan.nextLine();
+System.out.println("Hello " + s + "!");
 }
+}
+
+
