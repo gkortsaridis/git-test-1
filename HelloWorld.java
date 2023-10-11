8d57cabf43d88edda3e6e4b9a6e438ef2c7c7b7b
George Kortsaridis <gkortsaridis@gmail.com>
2023-10-11 16:03:12 +0100

    Added git checkout maingit checkout main

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,11 @@
 public class HelloWorld {
-public static void main(String[] args) {
-java.util.Scanner scan = new java.util.Scanner(System.in);
-System.out.println("Hello World!");
-System.out.println("What’s your name?");
-String s = scan.nextLine();
-System.out.println("Hello " + s + "!");
-}
+    public static void main(String[] args) {
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+        System.out.println("Hello World!!!!!");
+        System.out.println("What’s your name?");
+        String s = scan.nextLine();
+        System.out.println("Hello " + s + "!");
+    }
 }
 
 
